4d4df3356ba6f0d146900c180bb42176c68245c0
Pablo <18240248@leon.tecnm.mx>
2021-05-10 19:45:00 -0500

    feat: Include run example

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,28 @@
 package main;
 
+import automata.Automata;
+import automata.AutomataBuilder;
+import automata.operations.Equivalence;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: Run program.
+        Automata automata = new AutomataBuilder()
+            .addState("a")
+            .addState("b")
+            .addTransition("a", 'a', "b")
+            .addTransition("b", 'a', "a")
+            .setInitialState("a")
+            .setFinalState("a")
+            .get();
+
+        Automata automata2 = new AutomataBuilder()
+            .addState("a")
+            .addTransition("a", 'a', "a")
+            .setInitialState("a")
+            .setFinalState("a")
+            .get();
+
+        System.out.println(Equivalence.test(automata, automata2));
+
     }
 }
