02cf8b45fbe106d319a459bee196bbf52704253a
Pablo <18240248@leon.tecnm.mx>
2021-05-13 14:29:30 -0500

    feat: Print result table

--- src/automata/operations/Equivalence.java
+++ src/automata/operations/Equivalence.java
@@ -10,9 +10,9 @@ import automata.Automata;
 import automata.State;
 
 public class Equivalence {
-    public static boolean test(Automata m1, Automata m2) {
+    public static EquivalenceResult test(Automata m1, Automata m2) {
         if (!compareAlphabets(m1.getAlphabet(), m2.getAlphabet()))
-            return false;
+            return new EquivalenceResult(false);
 
         return verifyWithTable(m1, m2);
     }
@@ -21,35 +21,45 @@ public class Equivalence {
         return a1.containsAll(a2) && a2.containsAll(a1);
     }
 
-    private static boolean verifyWithTable(Automata m1, Automata m2) {
+    private static EquivalenceResult verifyWithTable(Automata m1, Automata m2) {
+        EquivalenceResult res = new EquivalenceResult(m1.getAlphabet());
         Set<String> seen = new HashSet<>();
         Queue<Pair<State>> pairs = new LinkedList<>();
 
-        Pair<State> initials = new Pair<>(
-                m1.getInitialState(),
-                m2.getInitialState()
-                );
-        if (!compatible(initials)) return false;
-        pairs.add(initials);
-        seen.add(initials.toString());
+        Pair<State> initialPair =
+            new Pair<>(m1.getInitialState(), m2.getInitialState());
+
+        if (!visitPair(seen, pairs, initialPair)) {
+            return res;
+        }
 
         while (!pairs.isEmpty()) {
             Pair<State> current = pairs.poll();
 
+            res.addRow(current);
             for (Character symbol : m1.getAlphabet()) {
                 Pair<State> next = new Pair<>(
                         current.first().transition(symbol),
                         current.second().transition(symbol)
                         );
 
-                if (!compatible(next)) return false;
-
-                if (seen.contains(next.toString())) continue;
-
-                seen.add(next.toString());
-                pairs.add(next);
+                res.addHop(next);
+                if (!visitPair(seen, pairs, next)) {
+                    return res;
+                }
             }
         }
+
+        res.setEquivalent(true);
+        return res;
+    }
+
+    private static boolean visitPair(Set<String> seen, Queue<Pair<State>> pairs, Pair<State> pair) {
+        if (!compatible(pair)) return false;
+        if (seen.contains(pair.toString())) return true;
+
+        pairs.add(pair);
+        seen.add(pair.toString());
 
         return true;
     }
---
+++ src/automata/operations/EquivalenceResult.java
@@ -0,0 +1,66 @@
+package automata.operations;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import automata.State;
+import utils.Pair;
+
+public class EquivalenceResult {
+    private boolean equivalent = false;
+
+    private ArrayList<ArrayList<Pair<State>>> table;
+    private int rowIdx = -1;
+
+    private Set<Character> alphabet;
+
+    public EquivalenceResult(Set<Character> alphabet) {
+        table = new ArrayList<>();
+        this.alphabet = alphabet;
+    }
+
+    public EquivalenceResult(boolean equivalent) {
+        table = new ArrayList<>();
+        this.equivalent = equivalent;
+    }
+
+    public void addRow(Pair<State> pair) {
+        ArrayList<Pair<State>> row =  new ArrayList<>();
+
+        row.add(pair);
+        table.add(row);
+        ++rowIdx;
+    }
+
+    public void addHop(Pair<State> pair) {
+        table.get(rowIdx).add(pair);
+    }
+
+    public void setEquivalent(boolean equivalent) {
+        this.equivalent = equivalent;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder repr = new StringBuilder();
+
+        repr.append("M1\tM2\t");
+        for (Character a : alphabet) {
+            repr.append(a + "(M1)\t" + a + "(M2)\t");
+        }
+        repr.append("\n");
+
+        for (ArrayList<Pair<State>> col : table) {
+            col.stream()
+                .forEach(p -> repr.append(p.first().getName() + "\t" + p.second().getName() + "\t"));
+            repr.append("\n");
+        }
+
+        repr.append("\n");
+        repr.append((equivalent)
+                ? "Los automatas son equivalentes\n"
+                : "Los automatas no son equivalentes\n");
+
+        return repr.toString();
+    }
+}
